74ee0dfb8e09c8d83b49453cb614e3cd38cbd1d0
Ash Ryan Arnwine <accounts@ashryan.io>
2020-06-21 15:17:06 -0400

    Initial functionality for volunteer dispact feature.

---
+++ index.js
@@ -0,0 +1,48 @@
+const PNF = require("google-libphonenumber").PhoneNumberFormat;
+const phoneUtil = require("google-libphonenumber").PhoneNumberUtil.getInstance();
+const numbers = require("./numbers");
+
+/**
+ * Parse phone numbers
+ *
+ * @param {string} rawInput - phone number to operate on.
+ * @returns {string} - formatted phone number.
+ */
+const getTappablePhoneNumber = (rawInput) => {
+  let parsedNumber;
+  try {
+    parsedNumber = phoneUtil.parseAndKeepRawInput(rawInput, "US");
+  } catch (error) {
+    return false; // Not a phone number
+  }
+
+  if (!phoneUtil.isValidNumber(parsedNumber)) {
+    return false; // Not a phone number
+  }
+
+  // Return +1 ###-###-#### without the country code
+  return phoneUtil.format(parsedNumber, PNF.INTERNATIONAL).substring(3);
+};
+
+/**
+ * Format phone numbers
+ *
+ * @param {string} rawInput - phone number to operate on.
+ * @returns {string} - formatted phone number.
+ */
+const getDisplayNumber = (rawInput) => {
+  if (!rawInput) return "None provided";
+
+  const tappableNumber = getTappablePhoneNumber(rawInput);
+
+  const displayNumber =
+    tappableNumber || `${rawInput} _[Bot note: unparseable number.]_`;
+
+  return displayNumber;
+};
+
+const displayNumbers = numbers.map((number) => {
+  return getDisplayNumber(number);
+});
+
+console.log(displayNumbers);
